1ce87354f3ec3e54a8deead75556f959624c1341
kasyap-007 <rishikasyap007@gmail.com>
2022-07-14 01:25:21 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="bg-gray-200 min-h-screen w-full bg-[img('https://www.freepik.com/photos/star-sky)]  ">
+      <div className="bg-gradient-to-r from-sky-400 container mx-auto to-purple-500 text-white rounded-lg text-center">
+        <h1 className="text-7xl">STAR WARS</h1>
+        <p className="capitalize">May the force be with you</p>
+      </div>
     </div>
   );
 }
